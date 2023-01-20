18fcc1ef0ef162f935a1779deae287248be19012
vinknie <64844166+vinknie@users.noreply.github.com>
2023-01-20 10:15:26 +0100

    formquizz

--- resources/views/pages/quizz.blade.php
+++ resources/views/pages/quizz.blade.php
@@ -5,10 +5,12 @@
     $values = explode("," , $qcm->Option);
     $valuesId = explode("," , $qcm->IdOption)
 @endphp --}}
-
+<form class="" action="" method="post" enctype="multipart/form-data">
+    @csrf
 <div>
     <h1>Quizz {{ $qcm[0]->titre }} </h1>
 </div>
+<form>
 @foreach ($qcm as $qcms)
 
     {{ $qcms->question }}<br>
@@ -19,10 +21,15 @@
     <ul class="list-group">
         @foreach (array_combine($values, $valuesId) as $value => $id )
 
-        <li class="list-group-item mb-1"><input class="form-check-input me-1" type="checkbox" id="{{ $id }}" name="checkbox[]">{{ $value }}</li>
+        <li class="list-group-item mb-1">
+            <input class="form-check-input me-1" type="checkbox" id="{{ $id }}" name="answers[{{$qcms->id}}][]" value="{{ $id }}">
+            <label for="{{ $id }}">{{ $value }}</label>
+        </li>
         @endforeach
     </ul>
 @endforeach
+<input type="submit" value="submit">
+</form>
 
 
 @endsection
